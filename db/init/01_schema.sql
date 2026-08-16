fe1b5e7ac7031af6ffcb8184c684ceea8eada0ee
Lakshya upadhyay <143817471+hackbuddy1@users.noreply.github.com>
2026-08-16 19:46:30 +0530

    Update 01_schema.sql

--- db/init/01_schema.sql
+++ db/init/01_schema.sql
@@ -1,12 +1,12 @@
 CREATE EXTENSION IF NOT EXISTS timescaledb;
 
 CREATE TABLE ticks (
-    ts     TIMESTAMPTZ      NOT NULL,
-    symbol TEXT             NOT NULL,
-    price  DOUBLE PRECISION NOT NULL,
-    qty    INTEGER          NOT NULL,
-    bid    DOUBLE PRECISION NOT NULL,
-    ask    DOUBLE PRECISION NOT NULL
+    ts TIMESTAMPTZ NOT NULL,
+    symbol TEXT NOT NULL,
+    price DOUBLE PRECISION NOT NULL,
+    qty INTEGER NOT NULL,
+    bid DOUBLE PRECISION NOT NULL,
+    ask DOUBLE PRECISION NOT NULL
 );
 
 SELECT create_hypertable('ticks', 'ts', chunk_time_interval => INTERVAL '1 hour');
